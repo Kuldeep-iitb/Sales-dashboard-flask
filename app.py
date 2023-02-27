51e19536876a4c0986d654ff726789b4d3ad83c0
joakimeineving <Joakim.eineving@gmail.com>
2023-02-27 10:00:31 +0100

    performance opt

--- app.py
+++ app.py
@@ -3,23 +3,14 @@ from flask import Flask, render_template, url_for, request
 import apikey as apikey
 import requests
 import json
-from datetime import datetime, timedelta, date, time
+from datetime import datetime, timedelta
 import logging
 
 logging.basicConfig(level=logging.DEBUG)
 
 app = Flask(__name__, static_url_path='/static')
 
-# get current date minus one year
-def one_year():
-    today = datetime.today()
-    year = timedelta(days=365)
-    return today - year
 
-def one_month():
-    today = datetime.today()
-    month = timedelta(days=30)
-    return today - month
 
 headers = {
     "Content-Type": "application/json",
@@ -27,8 +18,6 @@ headers = {
     "x-api-key": apikey.apikey
 }
 
-
-# Example of function for REST API call to get data from Lime
 def get_api_data(headers, url):
     
     response = requests.get(url=url,
@@ -39,7 +28,7 @@ def get_api_data(headers, url):
 
     json_data = json.loads(response.text)
     limeobjects = json_data.get("_embedded").get("limeobjects")
-
+    
     # Check for more data pages and get thoose too
     nextpage = json_data.get("_links").get("next")
     while nextpage is not None:
@@ -54,9 +43,8 @@ def get_api_data(headers, url):
         nextpage = json_data.get("_links").get("next")
 
     return limeobjects
-# Index page
+
 def get_api_data_next(headers, url):
-    # First call to get first data page from the API
     response = requests.get(url=url,
                             headers=headers,
                             data=None,
@@ -65,13 +53,18 @@ def get_api_data_next(headers, url):
     # Convert response string into json data and get embedded limeobjects
     json_data = json.loads(response.text)
     limeobjects = json_data.get("_embedded").get("limeobjects")
-    # Check for more data pages and get thoose too
+    
     nextpage = json_data.get("_links").get("next")
     for item in limeobjects:
         item['next'] = nextpage
-
-
     return limeobjects
+
+# get current date minus one year
+def one_year():
+    today = datetime.today()
+    year = timedelta(days=365)
+    return today - year
+
 
 
 @app.route('/')
@@ -83,41 +76,47 @@ def dashboard():
 
     deals = get_api_data(headers, url)
     
-    companies = [deal.get("_embedded").get("relation_company") for deal in deals]
-    print(companies[0])
+    companies = [deal["_embedded"]["relation_company"] for deal in deals]
     
-  
-    
+    # Initialize variables
     company_totals = {}
     companies_with_deals = {}
-
-    months = [datetime.today() - timedelta(days=30*i) for i in range(13)]
-    deals_month = Counter({m.strftime('%Y-%m'): 0 for m in months})
     deal_value = 0
     deals_year = 0
 
-    # deals_month = []
+    #returns array of ex. (2023, 2, 27, 9, 20, 10, 461091)
+    months = [datetime.today() - timedelta(days=30*i) for i in range(13)] 
+    #converts months to strings and assign value 0 ex. "2023-02': 0"
+    deals_month = Counter({m.strftime('%Y-%m'): 0 for m in months}) 
+    
+
+    
     for deal in deals:
+        # extract the company ID and deal value
         deal_id = deal['company']
         value = deal['value']
+        # update total deals/year and deal value/year
         deals_year += 1
-        deal_value += deal.get("value")
-        date = deal.get("closeddate").split("-")
+        deal_value += deal['value']
+        # get date in correct format
+        date = deal['closeddate'].split("-")
         date = date[0] + "-" + date[1]
+        # increment ammount of deals at current month
         if date in deals_month:
             deals_month[date] += 1
+        # update total value for each company
         if deal_id in company_totals:
             company_totals[deal_id] += value
         else:
             company_totals[deal_id] = value
-
-    deals_month = Counter(deals_month)
+    
+    
     deals_month = json.dumps(deals_month)
     deal_value_year = int(deal_value)
     deal_value = int(deal_value / len(deals))
     customers_year = len(company_totals)
 
-
+    # update the companies_with_deals dictionary with the total value of deals for each company
     for company in companies:
         company_id = company['_id']
         if company_id in company_totals:
@@ -126,30 +125,32 @@ def dashboard():
             companies_with_deals[name] = total_value
 
     companies_with_deals = json.dumps(companies_with_deals)
+    print(companies_with_deals)
+    print(company_totals)
 
     return render_template('dashboard.html', deal_value=deal_value, deal_value_year=deal_value_year, deals_month=deals_month, deals_year=deals_year, customers_year=customers_year, companyTotalYear=companies_with_deals)
 
 
 @app.route('/customers')
 def customers():
+    limit = 35 # set limit of data to get from API
     offset = request.args.get('offset', default=0, type=int)
     base_url = "https://api-test.lime-crm.com/api-test/api/v1/limeobject/company/"
-    params = f"?_limit=35&_sort=-_timestamp&_offset={offset}"
+    params = f"?_limit={limit}&_sort=-_timestamp&_offset={offset}"
     url = base_url + params
+
     company_res = get_api_data_next(headers, url)
+
     base_url = "https://api-test.lime-crm.com/api-test/api/v1/limeobject/deal/"
-    params = f"?_limit=35&_sort=-closeddate&dealstatus=agreement&_offset={offset}"
+    params = f"?_limit={limit}&_sort=-closeddate&dealstatus=agreement&_offset={offset}"
     url = base_url + params
 
     deals_res = get_api_data_next(headers, url)
-    print(len(company_res))
    
-    next_offset = offset + 35 if len(company_res) == 35 else None
-    prev_offset = offset - 35 if offset > 0 else None
-    if len(company_res) == 35: print('we have 30')
-    print(next_offset)
+    next_offset = offset + limit if len(company_res) == limit else None
+    prev_offset = offset - limit if offset > 0 else None
+   
     
-    #get 'company' from deals_res and compare to 'company_id' in company_res where closeddate is within the last year
     deals_filtered = [{"company": deal["company"],"value": deal["value"] , "closeddate": deal["closeddate"] } for deal in deals_res]
     deals_last_year = [deal for deal in deals_filtered if deal["closeddate"] > one_year().strftime("%Y-%m-%d")]
 
@@ -158,9 +159,8 @@ def customers():
     customers = []
     prospects = []
     inactive = []
-    #sort customers by value
     
-    #check if company_id is in deals_last_year
+    # Iterate over the companies and assign a status to each one based on whether it has had any deals in the last year
     for company in companies_filtered:
         if company["company_id"] in [deal["company"] for deal in deals_last_year]:
             company["status"] = "customer"
@@ -177,9 +177,10 @@ def customers():
             company["status"] = "inactive"
             inactive.append(company)
 
+    # Sort the customers by by deal value
     customers.sort(key=lambda x: x["value"], reverse=True)
     
-    companies = customers + prospects + inactive   
+    companies = customers + prospects + inactive
   
 
     return render_template('customers.html', companies=companies, next_offset=next_offset, prev_offset=prev_offset)
